ceb1ed46e060cde8ebf14cfc4564ced37d298dc7
Ilia Baryshnikov <qwelias@gmail.com>
2022-07-26 00:00:45 +0200

    fix: improve types and readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# setch
+**s**implest f**etch** wrapper I could come up with to reduce boilerplate code when using **fetch** on a daily basis while keeping as much compatibility with standatd as possible
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 /**
  * Make setch function with baseUrl, baseOptions, baseStatus
+ *
+ * @param baseUrl url string to use as a base
+ * @param baseOptions request options to use as a base
+ * @param baseStatus expected base status code
+ * @returns `setch` function where you can append to `baseUrl`,
+ * merge extra options (`headers` merged separately, so it's required to be a `Record`),
+ * or use another expected status
  */
  export const makeSetch = (
     baseUrl: string,
-    baseOptions: RequestInit = {},
+    baseOptions: SetchInit = {},
     baseStatus?: Status,
 ): typeof setch => (url, options = {}, status?) => setch(
     baseUrl + url,
@@ -12,9 +19,15 @@
 )
 
 /**
- * Make fetch and throw on unexpected status code
+ * fetch and throw on unexpected status code
+ *
+ * @param url
+ * @param options same as `RequestInit`, except `headers` have to be a `Record` so it's easy to merge them with ones from `baseOptions`
+ * @param status expected response status code.
+ * Either number or RegExp to match,
+ * if true uses Response.ok, if false no check is made
  */
-export const setch = async (url: string, options: RequestInit = {}, status: Status = true) => {
+export const setch = async (url: string, options: SetchInit = {}, status: Status = true) => {
     const res = await fetch(url, options)
 
     if (!isOk(status, res)) throw Object.assign(
@@ -25,6 +38,7 @@ export const setch = async (url: string, options: RequestInit = {}, status: Stat
     return res
 }
 
+export type SetchInit = Omit<RequestInit, 'headers'> & { headers?: Record<string, string> }
 export type Status = number | RegExp | boolean
 export const isOk = (status: Status, res: Response) => {
     if (status === false) return true
@@ -33,6 +47,9 @@ export const isOk = (status: Status, res: Response) => {
     return res.status === status
 }
 
+/**
+ * Just some shortcuts for use in `content-type` or `accept` headers
+ */
 export const mime = {
     json: 'application/json; charset=utf-8',
     urlencoded: 'application/x-www-form-urlencoded; charset=utf-8',
